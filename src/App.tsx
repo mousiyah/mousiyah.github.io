cc8978860700822310f030ecab61cf664e586bae
mousiyah <muslimakarimova@gmail.com>
2025-09-01 01:54:50 +0500

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import "./App.css";
 import Header from "./partials/Header";
 
 function App() {
+  console.log("App component rendering");
+
   return (
     <div className="App">
+      <div style={{ padding: "20px", background: "red", color: "white" }}>
+        Debug: App is rendering
+      </div>
       <Header />
     </div>
   );
