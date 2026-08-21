d01a7c119884287cc8d3092a6f9ae06d3029bb60
mousiyah <muslimakarimova@gmail.com>
2026-08-21 13:22:00 +0100

    experience

--- src/ui/RotatingCat.tsx
+++ src/ui/RotatingCat.tsx
@@ -52,6 +52,21 @@ export default function RotatingCat() {
       >
         {isPlaying ? <Volume2 size={20} /> : <VolumeX size={20} />}
       </button>
+      <div className="voice-hint" aria-hidden="true">
+        <svg
+          className="voice-hint__curve"
+          viewBox="0 -16 64 52"
+          role="presentation"
+        >
+          <path d="M 60 32 C 52 10, 30 2, 6 -12" />
+          <path d="M 15 -12 L 6 -12 L 9 -4" />
+        </svg>
+        <span>
+          click here for
+          <br />
+          full experience
+        </span>
+      </div>
       <audio ref={audioRef} src={oiiaAudio} loop preload="none" />
     </>
   );
